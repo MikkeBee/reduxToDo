55b25839e9076bc47a980d8844d70c7e92b91a03
MikkeBee <mjblakley87@gmail.com>
2022-09-01 15:35:07 +0300

    progress with todo redux practice

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import taskItem from "./taskItem";
 
 const List = () => {
-  return <div></div>;
+  const items = useSelector((state) => {
+    return state.content;
+  });
+
+  return (
+    <div>
+      <ul className="tasks-list">
+        {items.map((item) => (
+          <taskItem id={item.id} title={item.name} completed={item.status} />
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default List;
---
+++ src/components/taskItem.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const taskItem = () => {
+  return <div></div>;
+};
+
+export default taskItem;
